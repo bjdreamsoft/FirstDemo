9504639e3e555dcc5a48c755bfc369d7b3c924bc
ujnwa <ujnwa@DESKTOP-DLIE41H>
2018-07-31 20:47:36 +0800

    Change-Id: I2c9834bbbc1d45743a9a361690ce239611480dc7 Signed-off-by: ujnwa <ujnwa@DESKTOP-DLIE41H>

--- src/main/java/wanghl/com/firstDemo/HelloWorld.java
+++ src/main/java/wanghl/com/firstDemo/HelloWorld.java
@@ -3,6 +3,7 @@ package wanghl.com.firstDemo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("3333333333");
 		System.out.println("hello world");
 	}
 	
